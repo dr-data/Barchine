deb384adcab9ebba6b5fea32bfc9f711a34afd21
Diego Bonilla <diegobonilla004@gmail.com>
2016-06-12 20:29:55 -0400

    Add files via upload

---
+++ barchine_hub.py
@@ -0,0 +1,66 @@
+#Diego Bonilla 2016 Bartending machine main hub
+
+#Import the py script which handles logic
+import barchine_logic
+import sys
+
+#Commands: load,save,verify,list,edit,help,exit
+
+#Launch print out
+print 'Barchine::Made by Diego Bonilla, 2016'
+print 'type "help" to list all commands'
+user = ''
+#Main console loop
+while(user!='exit'):
+
+    #Display warning for empty data sets
+    if(barchine_logic.empty_liquids()==True):
+        print '#### NO WAREHOUSE LOADED ####'
+    if(barchine_logic.empty_menu()==True):
+        print '#### NO MENU LOADED ####'
+    
+    #get user input
+    user = raw_input('')
+    #run loading function
+    if (user=='load'):
+        barchine_logic.load_warehouse()
+        barchine_logic.load_menu()
+    #run saving function
+    if (user=='save'):
+        if(barchine_logic.saveData()==True):
+            print 'Data saved'
+    #run verify function
+    if (user=='verify'):
+        barchine_logic.verify()
+    #run console data listing function
+    if (user=='list'):
+        barchine_logic.print_liquids()
+        print ''
+        if(barchine_logic.validMenu()==True):
+            barchine_logic.print_menu()
+    #run edit mode
+    if(user=='edit'):
+        var = raw_input('edit "ingredient" or "drink"?: ')
+        if(var=='drink'):
+            while True:
+                if(barchine_logic.edit_menu()==True):
+                    break
+        else:
+            while True:
+                if(barchine_logic.edit_liquid()==True):
+                    break
+    #print out list of commands with description
+    if (user=='help'):
+        print 'load - Load data for ingredients from warehouse.txt'
+        print '     - Load data for drinks from menu.txt'
+        print 'save - Save drinks menu and ingredients to a file'
+        print 'verify - Check for low stock and invalid drink recipes'
+        print 'list - List a full ingredients list and menu list with relevant data points'
+        print 'edit - Edit entries for both drinks and ingredients'
+        print 'help - List all console commands with description'
+        print 'exit - Exit and close program'
+sys.exit
+
+
+
+
---
+++ barchine_logic.py
@@ -0,0 +1,250 @@
+#Diego Bonilla 2016 Bartending machine logic
+import os.path
+import sys
+
+#create structure for ingredients
+# Family (Alcohol or Mixer), Name of ingredient, Amount in system, Positioning based on hardware setup, Cost per 25mL
+class ingredient(object):
+    def __init__(self,family,name,amount,pos,cost):
+        self.family=family
+        self.name=name
+        self.amount=amount
+        self.pos=pos
+        self.cost=cost
+        
+#create structure for drinks
+#Name of drink, array of drink ingredients (See next structure)
+class drink(object):
+    def __init__(self,name,recipe):
+        self.name=name
+        self.recipe=recipe
+        
+#create drink recipe ingredients structure
+#Name of ingredient, Amount designated by drink (mL)
+class part(object):
+    def __init__(self,name,amount):
+        self.name=name
+        self.amount=amount
+        
+#global variables to store ingredients list and menu list
+liquids = []
+menu = []
+
+#load data from warehouse save file
+def load_warehouse():
+    print 'checking warehouse'
+    if os.path.isfile('warehouse.txt'):
+        print 'warehouse found'
+        f = open('warehouse.txt', 'r')
+        print 'generating liquids list'
+        for line in f:
+            data = line.split('|')
+            new = ingredient(data[0],data[1],int(data[2]),int(data[3]),float(data[4]))
+            liquids.append(new)
+    else:
+        print 'warehouse not found!'
+        sys.exit
+
+#load data from menu save file
+def load_menu():
+    print 'checking menu'
+    if os.path.isfile('menu.txt'):
+        print 'menu found'
+        #get total lines
+        num_lines=0
+        with open('menu.txt') as infp:
+            for line in infp:
+               if line.strip():
+                  num_lines += 1
+        f = open('menu.txt', 'r')
+        print 'generating menu list'
+        for num in range(0,num_lines/2):
+            recipe = []
+            title = f.readline().rstrip('\n')
+            #print 'adding '+title
+            line = f.readline()
+            data = line.split('|')
+            #print data
+            for num2 in range(0,len(data)-1,2):
+                recipe.append(part(data[num2],int(data[num2+1])))
+                #print data[num2]+' '+data[num2+1]
+            menu.append(drink(title,recipe))
+    else:
+        print 'menu not found!'
+        sys.exit
+
+#Verification call to verify the various aspects of the system data
+def verify():
+    print'Verifying...'
+    print'----------Low Volumes----------'
+    stockLevels()
+    print'----------Invalid Menu Choices----------'
+    validMenu()
+    
+#Check for low stock levels
+def stockLevels():
+    tally = 0
+    for num in range(len(liquids)):
+        if(liquids[num].family=='alcohol'):
+            if(liquids[num].amount<=75):
+                print str(liquids[num].name)+' '+str(liquids[num].amount)+'ml'
+                tally+=1
+        else:
+            if(liquids[num].amount<=200):
+                print str(liquids[num].name)+' '+str(liquids[num].amount)+'ml'
+                tally+=1
+    if(tally==0):print 'None Low'
+
+#ensure that menu items are valid and can be made with provided liquids
+def validMenu():
+    tally = 0
+    for num in range(len(menu)):
+        for num2 in range(len(menu[num].recipe)):
+            if(getLevel(menu[num].recipe[num2].name)<menu[num].recipe[num2].amount):
+                print str(menu[num].name)+' is missing '+str(menu[num].recipe[num2].name)
+                tally+=1
+    if(tally==0):
+        return True
+    
+#retrieve level from stock given name
+def getLevel(name):
+    for num in range(len(liquids)):
+        if(liquids[num].name==name):return liquids[num].amount
+        
+#retrieve cost from stock given name
+def getCost(name):
+    for num in range(len(liquids)):
+        if(liquids[num].name==name):return liquids[num].cost
+
+#Edit a drinks information
+def edit_drink():
+    var = raw_input('Enter name of drink to edit: ')
+    for num in range(len(menu)):
+            if(var==menu[num].name):
+                print'You selected: '+str(menu[num].name)
+                #Create backup
+                backup_obj = menu[num]
+                backup_index = num
+                print'Enter "exit" to leave edit mode'
+                while(var!='exit'):
+                    var = raw_input('Enter one of the following to edit: name,recipe: ')
+                    if(var=='name'):
+                        var = raw_input('Enter new name: ')
+                        menu[num].name = var
+                    if(var=='recipe'):
+                        for num2 in range(len(menu[num].recipe)):
+                            var = raw_input('Edit '+str(menu[num].recipe[num2].name)+'? (y/n): ');
+                            if(var=='y'):
+                                var = raw_input("Enter new name: ")
+                                menu[num].recipe[num2].name = var
+                                var = raw_input("Enter new amount (in mL): ")
+                                menu[num].recipe[num2].amount = int(var)
+                if(var=='exit'):
+                    return True
+    if(num==len(menu)):
+        print 'Ingredient DNE'
+        return False
+    var = raw_input('Save changes? (y/n): ')
+    if(var=='y'):
+        saveData()
+        return True
+    else:
+        menu[backup_index].name = backup_obj.name
+        menu[backup_index].recipe = backup_obj.recipe
+        return True
+    
+#Edit an ingredients information
+def edit_liquid():
+    found = False
+    var = raw_input('Enter name of ingredient: ')
+    for num in range(len(liquids)):
+            if(var==liquids[num].name):
+                found = True
+                print'You selected: '+str(liquids[num].name)
+                #Create backup
+                backup_obj = liquids[num]
+                backup_index = num
+                print'Enter "exit" to leave edit mode'
+                while(var!='exit'):
+                    var = raw_input('Enter one of the following to edit: family,name,amount,pos,cost: ')
+                    if(var=='family'):
+                        var = raw_input('Enter new family (mixer or alcohol): ')
+                        liquids[num].family = var
+                    if(var=='name'):
+                        var = raw_input('Enter new name: ')
+                        liquids[num].name = var
+                    if(var=='amount'):
+                        var = raw_input('Enter new amount (in mL): ')
+                        liquids[num].amount = int(var)
+                    if(var=='pos'):
+                        var = raw_input('Enter new pos (Reference machine labelling): ')
+                        liquids[num].pos = int(var)
+                    if(var=='cost'):
+                        var = raw_input('Enter new price (per 25mL): ')
+                        liquids[num].cost = float(var)
+    if(found==False):
+        print 'Ingredient DNE'
+        return False
+    var = raw_input('Save changes? (y/n): ')
+    if(var=='y'):
+        saveData()
+        return True
+    else:
+        liquids[backup_index].family = backup_obj.family
+        liquids[backup_index].name = backup_obj.name
+        liquids[backup_index].amount = backup_obj.amount
+        liquids[backup_index].pos = backup_obj.pos
+        liquids[backup_index].cost = backup_obj.cost
+        return True
+
+#calculate the total cost of a drink provided the name
+def calcCost(drink):
+    total=0
+    for num in range(len(drink.recipe)):
+        total += getCost(drink.recipe[num].name)*(drink.recipe[num].amount/25)
+    return 'Cost: $'+str(total)
+
+#List all data for menu items
+def print_menu():
+    print'-----Menu-----'
+    for num in range(len(menu)):
+            print ' '
+            print(str(menu[num].name))
+            print calcCost(menu[num])
+            for num2 in range(len(menu[num].recipe)):
+                print(str(menu[num].recipe[num2].name)+'::'+str(menu[num].recipe[num2].amount)+'mL')
+                
+#List all data for stored liquids
+def print_liquids():
+    print'-----Stock-----'
+    for num in range(len(liquids)):
+            print 'Type: '+str(liquids[num].family)+', Name: '+str(liquids[num].name)+', Amount: '+str(liquids[num].amount)+'mL, Storage Position: '+str(liquids[num].pos)+', Cost: $'+str(liquids[num].cost)
+            
+#Save all the ingredients information and menu information to the appropriate files
+def saveData():
+    f = open('warehouse.txt', 'w')
+    for num in range(len(liquids)):
+            f.write(str(liquids[num].family)+'|'+str(liquids[num].name)+'|'+str(liquids[num].amount)+'|'+str(liquids[num].pos)+'|'+str(liquids[num].cost)+'\n')
+    f.close()
+    f = open('menu.txt', 'w')
+    for num in range(len(menu)):
+        f.write(str(menu[num].name)+'\n')
+        for num2 in range(len(menu[num].recipe)):
+            f.write(str(menu[num].recipe[num2].name)+'|'+str(menu[num].recipe[num2].amount)+'|')
+        f.write('\n')
+    f.close()
+    return True
+
+#Check for empty liquids list
+def empty_liquids():
+    if(len(liquids)==0):
+        return True
+    else:
+        return False
+
+#Check for empty menu list
+def empty_menu():
+    if(len(menu)==0):
+        return True
+    else:
+        return False
